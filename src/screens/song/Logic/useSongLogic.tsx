94289dd11c82b93a9786d11745ec20a5de72da05
Deepanshu Tiwari <ddepu11@gmail.com>
2021-09-11 00:35:19 +0530

    refactor: move song logic in songLogic hook

---
+++ src/screens/song/Logic/useSongLogic.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from 'react';
+import {
+  playerPauses,
+  playerPlays,
+  playerSetCurrentSongAndPlaylist,
+} from '../../../features/player';
+import { ISong } from '../../../interfaces';
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
+
+const useSongLogic = (playlistId: string | undefined, song: ISong) => {
+  const dispatch = useAppDispatch();
+
+  const [isThisSongBeingplayed, setIsThisSongBeingplayed] =
+    useState<boolean>(false);
+
+  const [viewDashBoard, setViewDashBoard] = useState(false);
+
+  const { currentSong, play, pause } = useAppSelector(
+    (state) => state.player.value
+  );
+
+  const handlePauseSong = () => {
+    setIsThisSongBeingplayed(false);
+    dispatch(playerPauses());
+  };
+
+  const handlePlaySong = () => {
+    setIsThisSongBeingplayed(true);
+
+    if (currentSong !== song.url) {
+      dispatch(
+        playerSetCurrentSongAndPlaylist({
+          currentSong: song.url,
+          currentSongPic: song.pic.url,
+          currentSongName: song.name,
+          playlistId: playlistId,
+        })
+      );
+    }
+
+    dispatch(playerPlays());
+  };
+
+  useEffect(() => {
+    // Both conditions to sync global player play, pause buttons with this screen play,pause button
+    if (play && !pause && currentSong === song.url) {
+      setIsThisSongBeingplayed(true);
+    }
+
+    if (!play && pause && currentSong === song.url) {
+      setIsThisSongBeingplayed(false);
+    }
+
+    // THis is for when i play different song,this song's play button should display
+    if (currentSong !== song.url) {
+      setIsThisSongBeingplayed(false);
+    }
+  }, [play, pause, currentSong, song.url]);
+
+  // Confirming or denying song delete request
+  const showConfirmDialogBox = (): void => {
+    setViewDashBoard(true);
+  };
+  const hideConfirmDialogBox = (): void => {
+    setViewDashBoard(false);
+  };
+
+  return {
+    hideConfirmDialogBox,
+    showConfirmDialogBox,
+    isThisSongBeingplayed,
+    handlePlaySong,
+    handlePauseSong,
+    viewDashBoard,
+  };
+};
+
+export default useSongLogic;
--- src/screens/song/Song.tsx
+++ src/screens/song/Song.tsx
@@ -1,15 +1,10 @@
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 import styled from 'styled-components';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { AiOutlinePlayCircle, AiOutlinePauseCircle } from 'react-icons/ai';
 import { RiDeleteBin5Line } from 'react-icons/ri';
 import { ISong } from '../../interfaces';
-import {
-  playerPauses,
-  playerPlays,
-  playerSetCurrentSongAndPlaylist,
-} from '../../features/player';
 import Dialog from '../../components/Dialog';
+import useSongLogic from './Logic/useSongLogic';
 
 type Props = {
   playlistId: string | undefined;
@@ -26,62 +21,14 @@ const Song: FC<Props> = ({
   handleDeleteSong,
   playlistId,
 }): JSX.Element => {
-  const dispatch = useAppDispatch();
-
-  const [isThisSongBeingplayed, setIsThisSongBeingplayed] =
-    useState<boolean>(false);
-
-  const [viewDashBoard, setViewDashBoard] = useState(false);
-
-  const { currentSong, play, pause } = useAppSelector(
-    (state) => state.player.value
-  );
-
-  const handlePauseSong = () => {
-    setIsThisSongBeingplayed(false);
-    dispatch(playerPauses());
-  };
-
-  const handlePlaySong = () => {
-    setIsThisSongBeingplayed(true);
-
-    if (currentSong !== song.url) {
-      dispatch(
-        playerSetCurrentSongAndPlaylist({
-          currentSong: song.url,
-          currentSongPic: song.pic.url,
-          currentSongName: song.name,
-          playlistId: playlistId,
-        })
-      );
-    }
-
-    dispatch(playerPlays());
-  };
-
-  useEffect(() => {
-    // Both conditions to sync global player play, pause buttons with this screen play,pause button
-    if (play && !pause && currentSong === song.url) {
-      setIsThisSongBeingplayed(true);
-    }
-
-    if (!play && pause && currentSong === song.url) {
-      setIsThisSongBeingplayed(false);
-    }
-
-    // THis is for when i play different song,this song's play button should display
-    if (currentSong !== song.url) {
-      setIsThisSongBeingplayed(false);
-    }
-  }, [play, pause, currentSong, song.url]);
-
-  // Confirming or denying song delete request
-  const showConfirmDialogBox = (): void => {
-    setViewDashBoard(true);
-  };
-  const hideConfirmDialogBox = (): void => {
-    setViewDashBoard(false);
-  };
+  const {
+    hideConfirmDialogBox,
+    showConfirmDialogBox,
+    isThisSongBeingplayed,
+    handlePlaySong,
+    handlePauseSong,
+    viewDashBoard,
+  } = useSongLogic(playlistId, song);
 
   return (
     <Wrapper className='flex'>
