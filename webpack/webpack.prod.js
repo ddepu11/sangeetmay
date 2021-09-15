8d9a0afece424c04b6a4760abb36677f9fc6a98e
Deepanshu Tiwari <ddepu11@gmail.com>
2021-09-15 10:48:59 +0530

    fix: redirect to / on refresh on production

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,10 +1,22 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'production',
 
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../src/_redirects'),
+          to: path.resolve(__dirname, '../build'),
+        },
+      ],
+    }),
+  ],
 
   devtool: false,
 
