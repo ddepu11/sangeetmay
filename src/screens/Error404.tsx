fff3d7e54f9b0fa509ceccd644085d668b55880a
Deepanshu Tiwari <ddepu11@gmail.com>
2021-09-24 17:31:51 +0530

    style:  set width of error screen if role is admin

--- src/screens/Error404.tsx
+++ src/screens/Error404.tsx
@@ -1,12 +1,18 @@
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { useAppSelector } from '../redux/hooks';
 
-const Error404 = () => (
-  <Wrapper className='flex'>
-    <h2>Error 404! The page you have requested doesen&apos;t exists!</h2>
-  </Wrapper>
-);
+const Error404 = () => {
+  const { role } = useAppSelector((state) => state.user.value);
 
-const Wrapper = styled.main`
+  return (
+    <Wrapper className='flex' roleOfPerson={role}>
+      <h2>Error 404! The page you have requested doesen&apos;t exists!</h2>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.main<{ roleOfPerson: string | null }>`
   padding: 5px 5px;
   height: 76vh;
   width: 80%;
@@ -16,6 +22,12 @@ const Wrapper = styled.main`
     color: #b8b7b7;
     font-weight: 400;
     letter-spacing: 1px;
+  }
+
+  width: ${({ roleOfPerson }) => roleOfPerson && '100%'};
+
+  @media screen and (max-width: 929px) {
+    width: 100%;
   }
 `;
 
